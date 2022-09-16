25d7e86f9e937dbfbf5835147a65f2c4cb4d633d
Charlie <charlie.dubois@univ-lorraine.fr>
2022-09-16 15:45:53 +0200

    Test Utils class

--- src/test/java/fr/univlorraine/mondossierweb/test/utils/TestUtils.java
+++ src/test/java/fr/univlorraine/mondossierweb/test/utils/TestUtils.java
@@ -24,6 +24,11 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -34,6 +39,9 @@ import com.vaadin.flow.component.textfield.TextField;
 import fr.univlorraine.mondossierweb.utils.CSSColorUtils;
 import fr.univlorraine.mondossierweb.utils.CmpUtils;
 import fr.univlorraine.mondossierweb.utils.Utils;
+import fr.univlorraine.pegase.model.insgestion.CibleInscription;
+import fr.univlorraine.pegase.model.insgestion.Formation;
+import fr.univlorraine.pegase.model.insgestion.ObjetFormationOuGroupement;
 import lombok.extern.slf4j.Slf4j;
 
 /** Tests Utils.
@@ -45,7 +53,7 @@ class TestUtils {
 	
 	/** Test de la class CmpUtils. */
 	@Test
-	void testfFormatTextField() {
+	void testFormatTextField() {
 		TextField tf = new TextField();
 		CmpUtils.formatTextField(tf);
 		assertThat(tf.getStyle().get(CSSColorUtils.MARGIN), is(equalTo("0em")));
@@ -53,11 +61,61 @@ class TestUtils {
 	
 	/** Test de la class Utils. */
 	@Test
-	void testfDisplayNote() {
+	void testDisplayNote() {
 		assertEquals("15/20", Utils.displayNote(new BigDecimal(15),20, true));
 		assertEquals("10/20", Utils.displayNote(new BigDecimal(10),20, true));
 		assertEquals("12/20", Utils.displayNote(new BigDecimal("12.0"),20, true));
 		assertEquals("5.6/20", Utils.displayNote(new BigDecimal("5.6"),20, true));
 	}
+	
+	@Test
+	void testGetCodeChemin() {
+		CibleInscription cible = new CibleInscription();
+		Formation f = new Formation();
+		f.setCode("FORM");
+		cible.setFormation(f);
+		List<ObjetFormationOuGroupement> chemin = new LinkedList<>();
+		ObjetFormationOuGroupement obj1 = new ObjetFormationOuGroupement();
+		obj1.setCode("obj1");
+		chemin.add(obj1);
+		ObjetFormationOuGroupement obj2 = new ObjetFormationOuGroupement();
+		obj2.setCode("obj2");
+		chemin.add(obj2);
+		cible.setChemin(chemin);
+		assertEquals("FORM>obj1>obj2", Utils.getCodeChemin(cible));
+	}
+	
+	@Test
+	void testGetFileName() {
+		assertEquals("fichier", Utils.getFileName("fichier.txt"));
+		assertEquals(null, Utils.getFileName("fichier"));
+	}
+	
+	@Test
+	void testGetFileExtension() {
+		assertEquals(".txt", Utils.getFileExtension("fichier.txt"));
+		assertEquals(null, Utils.getFileExtension("fichier"));
+	}
+	
+	@Test
+	void testFormatDateToDisplay() {
+		LocalDate localDate = LocalDate.of(2015, 3, 25);
+		ZoneId defaultZoneId = ZoneId.systemDefault();
+		Date d = Date.from(localDate.atStartOfDay(defaultZoneId).toInstant());
+		assertEquals("25/03/2015", Utils.formatDateToDisplay(d));
+		assertEquals("", Utils.formatDateToDisplay(null));
+	}
+	
+	@Test
+	void testEstPassee() {
+		assertEquals(true, Utils.estPassee("1990-01-01"));
+		assertEquals(false, Utils.estPassee("2999-12-31"));
+	}
 
+	
+	@Test
+	void testFormatStringDateToDisplay() {
+		assertEquals("25/03/2022", Utils.formatStringDateToDisplay("2022-03-25"));
+		assertEquals("", Utils.formatStringDateToDisplay(""));
+	}
 }
