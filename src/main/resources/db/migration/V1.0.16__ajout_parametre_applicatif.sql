389f2542a355f2ef35f2a9243419b317a1edb343
Charlie <charlie.dubois@univ-lorraine.fr>
2025-04-28 09:19:24 +0200

    maj lib param bdd

--- src/main/resources/db/migration/V1.0.16__ajout_parametre_applicatif.sql
+++ src/main/resources/db/migration/V1.0.16__ajout_parametre_applicatif.sql
@@ -5,7 +5,7 @@
 --
 
 INSERT INTO preferences_application (pref_id, pref_desc, secret, valeur, cat_id, type_id, ordre)
-VALUES ('PEGASE_API_IDT_URL', 'Base Url de l''API du module IDT de Pégase (attention à conserver la structure de l''url d''exemple)', NULL, 'https://ins-pegase.fr/api/idt/v1', '3', 'STRING', 1);
+VALUES ('PEGASE_API_IDT_URL', 'Base Url de l''API du module IDT de Pégase (attention à conserver la structure de l''url d''exemple)', NULL, 'https://idt.pegase.fr/api/idt/v1', '3', 'STRING', 1);
 
 UPDATE preferences_application set pref_desc = 'Base Url de l''API du module PIECE EXT de Pégase (attention à conserver la structure de l''url d''exemple)'
 WHERE pref_id ='PEGASE_API_PIECE_EXT_URL';
@@ -37,3 +37,11 @@ WHERE pref_id ='PEGASE_TEST_PERIODE';
 UPDATE preferences_application set ordre = 10
 WHERE pref_id ='PEGASE_TEST_CHEMIN';
 
+UPDATE preferences_application set pref_desc = 'Code apprenant pour tester les appels aux APIs'
+WHERE pref_id ='PEGASE_TEST_APPRENANT';
+
+UPDATE preferences_application set pref_desc = 'Code période pour tester les appels aux APIs'
+WHERE pref_id ='PEGASE_TEST_PERIODE';
+
+UPDATE preferences_application set pref_desc = 'Chemin pour tester les appels aux APIs'
+WHERE pref_id ='PEGASE_TEST_CHEMIN';
